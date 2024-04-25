01b81ac9be4ab375ab2df71cf816c93dbc79f97c
Raphael de Souza Rico Santos <157643782+Raphael-Rico@users.noreply.github.com>
2024-04-25 17:16:56 -0300

    up

---
+++ exercicios/ex09/vot.js
@@ -0,0 +1,13 @@
+var age = 152
+if (age >=65){
+    console.log('Voto opcional!')
+}
+else if (age >=18){
+    console.log('É eleitor!')
+}
+else if (age >=16){
+    console.log('Voto opcional!')
+}
+else{
+    console.log('Idade insuficiente para votar!')
+}
