48a8a2ea821177abdd1fc8d3e9a97dafb7b62347
Raphael de Souza Rico Santos <157643782+Raphael-Rico@users.noreply.github.com>
2024-05-03 16:06:44 -0300

    up

--- exercicios/ex11/estilos/script.js
+++ exercicios/ex11/estilos/script.js
@@ -24,5 +24,30 @@ function acao(){
         var ml = window.document.getElementById('ide')
         ml.innerHTML= '(Essa pessoa é uma mulher)'
     }
+    if (sex.checked && res >=0 && res <=14){
+        var qq = document.getElementById('img')
+        qq.innerHTML= `<img id="bbm" src="estilos/babym.jpg" alt="Bebe Masculino">`
+    }
+    if(sex.checked && res >=15 && res <= 59){
+        var qqm = document.getElementById('img')
+        qqm.innerHTML=`<img id="hmi" src="estilos/manm.jpg" alt="Homem de meia idade">`
+    }
+    if (sex.checked && res >= 60 && res <= 129){
+        var qqv = document.getElementById('img')
+        qqv.innerHTML=`<img id="hv" src="estilos/manv.jpg" alt="Homem velho">`
+    }
+    var sexf = document.getElementById('iradio2')
+    if (sexf.checked && res >=0 && res <=14){
+        var qqu = document.getElementById('img')
+        qqu.innerHTML=`<img id="cm" src="estilos/babyw.webp" alt="Crinça menina">`
+    }
+    if (sexf.checked && res >=15 && res <= 59){
+        var mmi = document.getElementById('img')
+        mmi.innerHTML=`<img id="mum" src="estilos/womanm.jpg" alt="Mulher de meia idade">`
+    }
+    if (sexf.checked && res >= 60 && res <= 129){
+        var nsm = document.getElementById('img')
+        nsm.innerHTML=`<img id="nam" src="estilos/womanv.webp" alt="Mulher velha">`
+    }
 }
 
