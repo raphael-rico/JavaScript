6a146ac330773b91a2aaaf376349593e96afa982
Raphael de Souza Rico Santos <157643782+Raphael-Rico@users.noreply.github.com>
2024-05-15 23:12:49 -0300

    up

--- exercicios/ex15/codigo.js
+++ exercicios/ex15/codigo.js
@@ -6,3 +6,12 @@ function parimp(n){
         window.document.write(`<article style="height: 100vh; width: 100vw; background-color: darkcyan; text-align:center; padding-top: 30vh; font-size: 1.3em; ">O número digitado era ímpar</article>`)
     }
 }
+
+/*function fat(n){
+if (n == 1) {
+    return '1'
+}else{
+    return n*fat(n-1)
+}
+}
+console.log(fat(5))*/
