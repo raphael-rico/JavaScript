792ef1dbd7ee956ef9b1e8f57a263545781c3f07
Raphael de Souza Rico Santos <157643782+Raphael-Rico@users.noreply.github.com>
2024-05-15 21:12:23 -0300

    up

--- exercicios/ex14/codigo.js
+++ exercicios/ex14/codigo.js
@@ -1,7 +1,8 @@
 function acao (){
-let vetor = []
+var vetor = []
 for (c=0;c<5;c++){
     var res = document.getElementById("iform")
-    vetor[c.innerText] = res.value
+    vetor[c] = res.value
 }
-document.write(`Os valores dos inputs foram ${vetor[0].value}`)}
+document.write(`<p style="text-align: center; margin-top: 30vh; color: blue;">Os valores do input dentro do vetor de 3 elementos ficou preenchido com "${vetor[0]}" e "${vetor[1]}" e "${vetor[2]}".</p>`)}
+
